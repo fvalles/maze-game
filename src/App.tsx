df9c7fafb382209cda1e956fab5dcf30847f3b2f
Federico Valles <federico.e.valles@gmail.com>
2022-06-28 12:38:08 -0300

    generate maze package installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React, { FunctionComponent } from 'react';
+import generator from 'generate-maze';
 
-const App: FunctionComponent = () => (
-  <div>
-    <h1>Maze Game</h1>
-  </div>
-);
+const App: FunctionComponent = () => {
+  const maze = generator(4);
+  console.log(maze);
+
+  return (
+    <div>
+      <h1>Maze Game</h1>
+    </div>
+  );
+};
 
 export default App;
